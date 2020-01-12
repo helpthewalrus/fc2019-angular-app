e45a919bcd7765f6cdacd6dada24b6a53f67465c
Alina Tylets <alinatylets@gmail.com>
2020-01-12 19:21:40 +0300

    feat:  change app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainViewComponent } from "./main-view/main-view.component";
+import { LayoutModule } from "./layout/layout.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MainViewComponent],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([]),
+    LayoutModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
