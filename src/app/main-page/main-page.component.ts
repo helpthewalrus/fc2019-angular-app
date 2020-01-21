f5c0c719e5a4d28c1b11c28ea897164763cd6ac2
Alina Tylets <alinatylets@gmail.com>
2020-01-21 23:57:10 +0300

    feat: add load more button logic

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -13,7 +13,10 @@ export class MainPageComponent implements OnInit {
   public currentSourceId: string = "";
   public checkboxState: boolean = false;
   public sources = SOURCES;
-  public articles: any[] = [];
+  public articles: any[];
+  public allFoundArticles: any[];
+  public counter: number = 0;
+  public currentNewsFilter: string = "";
 
   constructor() {}
 
@@ -22,30 +25,52 @@ export class MainPageComponent implements OnInit {
   public onChangeCurrentSource({ name, id }) {
     this.currentSource = name;
 
-    this.articles = ARTICLES.reduce((acc, sourceArticles: any) => {
-      if (sourceArticles.source === id) {
+    this.allFoundArticles = ARTICLES.reduce((acc, sourceArticles: any) => {
+      if (sourceArticles.source === id || id === "all-sources") {
         return [...acc, ...sourceArticles.articles];
       }
       return acc;
     }, []);
-  }
+    this.articles = this.allFoundArticles.slice(0, 5);
+    this.counter = 5;
 
-  public onCurrentNewsFilterChange(currentNewsFilter: string): void {
-    this.articles = this.articles.filter((article: any) =>
-      article.title.includes(currentNewsFilter)
-    );
+    this.currentNewsFilter = "";
   }
 
   public onChangeCurrentCheckboxState(currentCheckboxState: boolean): void {
     this.checkboxState = currentCheckboxState;
-    console.log(currentCheckboxState);
 
-    this.articles = ARTICLES.reduce((acc, sourceArticles: any) => {
-      if (sourceArticles.source === "my-news") {
-        return [...acc, ...sourceArticles.articles];
-      }
-      return acc;
-    }, []);
-    console.log(this.articles);
+    if (currentCheckboxState === true) {
+      this.allFoundArticles = ARTICLES.reduce((acc, sourceArticles: any) => {
+        if (sourceArticles.source === "my-news") {
+          return [...acc, ...sourceArticles.articles];
+        }
+        return acc;
+      }, []);
+
+      this.articles = this.allFoundArticles.slice(0, 5);
+      this.counter = 5;
+    } else {
+      this.articles = [];
+      this.currentSource = "";
+    }
+
+    this.currentNewsFilter = "";
+  }
+
+  public onCurrentNewsFilterChange(currentNewsFilter: string): void {
+    this.articles = this.articles.filter((article: any) =>
+      article.title.toUpperCase().includes(currentNewsFilter.toUpperCase())
+    );
+    this.currentNewsFilter = currentNewsFilter;
+  }
+
+  public onLoadMoreButtonClicked(): void {
+    this.articles = [
+      ...this.articles.concat(
+        this.allFoundArticles.slice(this.counter, this.counter + 5)
+      )
+    ];
+    this.counter += 5;
   }
 }
