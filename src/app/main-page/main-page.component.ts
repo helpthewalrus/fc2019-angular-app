da21810c4d3dcceba7c57a68dd09df069038fce2
Alina Tylets <alinatylets@gmail.com>
2020-01-21 22:32:34 +0300

    feat: modified main-page and news-filter components

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+
+import { ARTICLES } from "../../assets/mock-data/articles";
+import { SOURCES } from "../../assets/mock-data/sources";
 
 @Component({
   selector: "app-main-page",
@@ -6,7 +9,43 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./main-page.component.scss"]
 })
 export class MainPageComponent implements OnInit {
+  public currentSource: string = "";
+  public currentSourceId: string = "";
+  public checkboxState: boolean = false;
+  public sources = SOURCES;
+  public articles: any[] = [];
+
   constructor() {}
 
   ngOnInit() {}
+
+  public onChangeCurrentSource({ name, id }) {
+    this.currentSource = name;
+
+    this.articles = ARTICLES.reduce((acc, sourceArticles: any) => {
+      if (sourceArticles.source === id) {
+        return [...acc, ...sourceArticles.articles];
+      }
+      return acc;
+    }, []);
+  }
+
+  public onCurrentNewsFilterChange(currentNewsFilter: string): void {
+    this.articles = this.articles.filter((article: any) =>
+      article.title.includes(currentNewsFilter)
+    );
+  }
+
+  public onChangeCurrentCheckboxState(currentCheckboxState: boolean): void {
+    this.checkboxState = currentCheckboxState;
+    console.log(currentCheckboxState);
+
+    this.articles = ARTICLES.reduce((acc, sourceArticles: any) => {
+      if (sourceArticles.source === "my-news") {
+        return [...acc, ...sourceArticles.articles];
+      }
+      return acc;
+    }, []);
+    console.log(this.articles);
+  }
 }
--- src/app/main-page/news-filter/news-filter.component.ts
+++ src/app/main-page/news-filter/news-filter.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
 
 @Component({
-  selector: 'app-news-filter',
-  templateUrl: './news-filter.component.html',
-  styleUrls: ['./news-filter.component.scss'],
+  selector: "app-news-filter",
+  templateUrl: "./news-filter.component.html",
+  styleUrls: ["./news-filter.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewsFilterComponent implements OnInit {
+  @Input() sources: any[];
+  @Input() currentSource: { name: string; id: string };
+  @Input() checkboxState: boolean;
 
-  constructor() { }
+  @Output() currentSourceChange: EventEmitter<any> = new EventEmitter();
+  @Output() currentNewsFilterChange: EventEmitter<string> = new EventEmitter();
+  @Output() currentCheckboxState: EventEmitter<boolean> = new EventEmitter();
+
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.sources);
   }
 
+  public onSourceChange({ target }) {
+    console.log("===============onSourceChange");
+    this.currentSourceChange.emit({
+      name: target.innerText,
+      id: target.dataset.srcid
+    });
+  }
+
+  public onEnterNewsFilter(currentNewsFilter: string): void {
+    console.log("===============onEnterNewsFilter");
+    this.currentNewsFilterChange.emit(currentNewsFilter);
+  }
+
+  public onCheckboxClicked({ target: { checked } }): void {
+    console.log("===============onCheckboxClicked");
+    this.currentCheckboxState.emit(checked);
+  }
 }
