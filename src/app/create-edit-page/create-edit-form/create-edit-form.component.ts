799d367056e1593bcc6ef2efcc65d1414a25f3b8
Alina Tylets <alinatylets@gmail.com>
2020-01-22 17:27:22 +0300

    feat: add logic to create edit component

--- src/app/create-edit-page/create-edit-form/create-edit-form.component.ts
+++ src/app/create-edit-page/create-edit-form/create-edit-form.component.ts
@@ -1,16 +1,67 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-create-edit-form',
-  templateUrl: './create-edit-form.component.html',
-  styleUrls: ['./create-edit-form.component.scss'],
+  selector: "app-create-edit-form",
+  templateUrl: "./create-edit-form.component.html",
+  styleUrls: ["./create-edit-form.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateEditFormComponent implements OnInit {
+  public articleForm = this.fb.group({
+    heading: ["", Validators.required],
+    shortDescription: [""],
+    content: ["", Validators.required],
+    photo: this.fb.group({
+      radioType: ["url"],
+      inputUrl: [""],
+      inputImg: [""]
+    }),
+    date: [""],
+    author: [""],
+    sourceUrl: [""]
+  });
 
-  constructor() { }
+  public hoho: boolean = false;
+  public image: string;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) {}
+
+  ngOnInit() {}
+
+  public onSubmit(): void {
+    console.log(this.articleForm.value);
   }
 
+  public onRadioClicked({ target: { value } }): void {
+    console.log(value);
+    this.hoho = value === "image" ? true : false;
+  }
+
+  public onFileSelect(event: any) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.articleForm.patchValue({ photo: { inputImg: file } });
+    this.photo.updateValueAndValidity();
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.image = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  public get photo(): any {
+    return this.articleForm.get("photo").get("inputImg");
+  }
+
+  public onCancel(): void {
+    console.log("=======onCancel=====");
+  }
 }
--- src/app/create-edit-page/create-edit-page.module.ts
+++ src/app/create-edit-page/create-edit-page.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { CreateEditPageRoutingModule } from './create-edit-page-routing.module';
-import { CreateEditPageComponent } from './create-edit-page.component';
-import { CreateEditFormComponent } from './create-edit-form/create-edit-form.component';
-
+import { CreateEditPageRoutingModule } from "./create-edit-page-routing.module";
+import { CreateEditPageComponent } from "./create-edit-page.component";
+import { CreateEditFormComponent } from "./create-edit-form/create-edit-form.component";
 
 @NgModule({
   declarations: [CreateEditPageComponent, CreateEditFormComponent],
-  imports: [
-    CommonModule,
-    CreateEditPageRoutingModule
-  ]
+  imports: [CommonModule, CreateEditPageRoutingModule, ReactiveFormsModule]
 })
-export class CreateEditPageModule { }
+export class CreateEditPageModule {}
