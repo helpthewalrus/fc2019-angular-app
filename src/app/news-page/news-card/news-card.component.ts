6cc961a8656cd7c3d135e12988bc188ceaed3fc8
Alina Tylets <alinatylets@gmail.com>
2020-01-22 23:21:33 +0300

    feat: change news card to use data service

--- src/app/news-page/news-card/news-card.component.ts
+++ src/app/news-page/news-card/news-card.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+
+import { Observable } from "rxjs";
+
+import { DataService } from "src/app/shared/services/data.service";
 
 @Component({
-  selector: 'app-news-card',
-  templateUrl: './news-card.component.html',
-  styleUrls: ['./news-card.component.scss'],
+  selector: "app-news-card",
+  templateUrl: "./news-card.component.html",
+  styleUrls: ["./news-card.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewsCardComponent implements OnInit {
+  public article: Observable<any>;
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
+    this.article = this.dataService.currentArticle;
   }
 
+  public onEditArticle(article: any) {
+    this.dataService.changeTitle("Edit");
+    this.dataService.setCurrentArticle(article);
+  }
 }
--- src/app/news-page/news-page-routing.module.ts
+++ src/app/news-page/news-page-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { NewsPageComponent } from './news-page.component';
+import { NewsPageComponent } from "./news-page.component";
 
-const routes: Routes = [{ path: '', component: NewsPageComponent }];
+const routes: Routes = [{ path: "", component: NewsPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class NewsPageRoutingModule { }
+export class NewsPageRoutingModule {}
