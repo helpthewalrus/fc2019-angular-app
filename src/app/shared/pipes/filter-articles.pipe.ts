ab67a76c97410cccb232fe34a86774c68b6b7e53
Alina Tylets <alinatylets@gmail.com>
2020-02-04 00:49:31 +0300

    feat: create filter pipe

--- src/app/shared/pipes/filter-articles.pipe.ts
+++ src/app/shared/pipes/filter-articles.pipe.ts
@@ -1,15 +1,19 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import { ArticleInterface } from "src/app/core/models";
 
 @Pipe({
   name: "filter"
 })
 export class FilterArticlesPipe implements PipeTransform {
-  transform(articles: any[], search: string = ""): any[] {
+  transform(
+    articles: ArticleInterface[],
+    search: string = ""
+  ): ArticleInterface[] {
     if (!search.trim()) {
       return articles;
     }
 
-    return articles.filter((article: any) =>
+    return articles.filter((article: ArticleInterface) =>
       article.title.toUpperCase().includes(search.toUpperCase())
     );
   }
