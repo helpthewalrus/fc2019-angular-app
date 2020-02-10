1c22a3d0927a9f4e4f04e6e2a204756fff12743c
Alina Tylets <alinatylets@gmail.com>
2020-02-10 23:28:21 +0300

    feat: add unit test to dataService partially

--- src/app/shared/services/data.service.spec.ts
+++ src/app/shared/services/data.service.spec.ts
@@ -1,12 +1,128 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 
-import { DataService } from './data.service';
+import { SourceInterface, ArticleInterface } from "src/app/core/models";
+import { DataService } from "./data.service";
 
-describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("DataService", () => {
+  let mockedSources: SourceInterface[] = [
+    {
+      id: "bbc-news",
+      name: "BBC News",
+      description: "BBC News",
+      url: "http://www.bbc.co.uk/news",
+      category: "general",
+      language: "en",
+      country: "gb"
+    },
+    {
+      id: "bbc-sport",
+      name: "BBC Sport",
+      description: "BBC Sport",
+      url: "http://www.bbc.co.uk/sport",
+      category: "sports",
+      language: "en",
+      country: "gb"
+    }
+  ];
 
-  it('should be created', () => {
-    const service: DataService = TestBed.get(DataService);
-    expect(service).toBeTruthy();
+  const article: ArticleInterface = {
+    _id: "111",
+    source: {
+      id: "my-news",
+      name: "My News"
+    },
+    author: "Test Author",
+    title: "First Article Title",
+    description: "Test Description",
+    url: "Test Url",
+    urlToImage: "Test Url To Image",
+    publishedAt: "2020-02-08T11:45:53Z",
+    content: "Test Content",
+    myNews: true
+  };
+
+  let dataService: DataService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(async () =>
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DataService]
+    }).compileComponents()
+  );
+
+  beforeEach(() => {
+    dataService = TestBed.get(DataService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  it("should be created", () => {
+    expect(dataService).toBeTruthy();
+  });
+
+  describe("changeAppTitle", () => {
+    beforeEach(() => {
+      dataService.changeAppTitle("Title");
+    });
+
+    it("current app title should equal to 'Title'", () => {
+      dataService.currentAppTitle$.subscribe((title: string) => {
+        expect(title).toEqual("Title");
+      });
+    });
+  });
+
+  describe("changeCurrentSource", () => {
+    beforeEach(() => {
+      dataService.changeCurrentSource("BBC Sport");
+    });
+
+    it("current articles source should equal to 'BBC Sport'", () => {
+      dataService.currentSource$.subscribe((source: string) => {
+        expect(source).toEqual("BBC Sport");
+      });
+    });
+  });
+
+  describe("changeCurrentArticlesFilter", () => {
+    beforeEach(() => {
+      dataService.changeCurrentArticlesFilter("Filter");
+    });
+
+    it("current articles filter should equal to 'Filter'", () => {
+      dataService.currentArticlesFilter$.subscribe((search: string) => {
+        expect(search).toEqual("Filter");
+      });
+    });
+  });
+
+  describe("changeIsOnlyMyArticles", () => {
+    beforeEach(() => {
+      dataService.changeIsOnlyMyArticles(true);
+    });
+
+    it("current flag isOnlyMyArticles is equal to true", () => {
+      dataService.isOnlyMyArticles$.subscribe((isOnlyMyArticles: boolean) => {
+        expect(isOnlyMyArticles).toEqual(true);
+      });
+    });
+  });
+
+  describe("setCurrentArticle", () => {
+    beforeEach(() => {
+      dataService.setCurrentArticle(article);
+    });
+
+    it("current flag isOnlyMyArticles is equal to true", () => {
+      dataService.currentArticle$.subscribe(
+        (currentArticle: ArticleInterface) => {
+          expect(currentArticle).toEqual(article);
+        }
+      );
+    });
   });
 });
