7373a816a8b7df61b7331edcd8218c8cd585e038
Alina Tylets <alinatylets@gmail.com>
2020-02-10 23:24:43 +0300

    feat: change api key

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,3 +1,3 @@
-export const API_KEY: string = "9149aea71b2f431a848506c2bdcef446";
+export const API_KEY: string = "a4545e5b1e104ab0800980dde3227d8c";
 export const BASE_URL: string = "https://newsapi.org/v2/";
 export const PAGE_SIZE: number = 5;
